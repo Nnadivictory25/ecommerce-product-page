fc742758850fc68379a0c3935ee8d54722060dea
Nnadi Victory <chikamagozi25@gmail.com>
2023-01-01 18:44:30 +0100

    project done and everything is working fine

--- app/js/script.js
+++ app/js/script.js
@@ -38,6 +38,7 @@ const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn')
 let currentImg = 1
 
+
 // ! Functions for image slide
 prevBtn.addEventListener('click', () => {
     currentImg--
@@ -56,12 +57,15 @@ let renderImg = () => {
 }
 
 
+
 // ! Desktop lightbox gallery variables
 const mainImgCtn = document.querySelector('.topImgCtn')
 const mainImg = mainImgCtn.firstElementChild
 const thumbnailImgs = Array.from(document.querySelectorAll('.thumImg__ctn'))
 let activeImg;
 let LBactiveImg;
+
+// * LB : LightBox
 
 const lightboxModal = document.querySelector('.modal')
 let closeLBmodalBtn = document.querySelector('#closeModal')
@@ -99,9 +103,11 @@ let openLBmodal = () => {
         img.addEventListener('click', () => {
             const innerImgSrc = img.firstElementChild.src
             const mainImgSrc = innerImgSrc.split('-').filter(word => word !== 'thumbnail.jpg').join('-')
+            let splitArr = mainImgSrc.split('-')
+            currentImgLB = +splitArr.at(-1)
             LbMainImg.src = `${mainImgSrc}.jpg`   
-            img.classList.add('active')
             LBactiveImg.classList.remove('active')
+            img.classList.add('active')
             LBactiveImg = img
         })
     }) 
@@ -156,4 +162,153 @@ let renderImgLB = () => {
     let currTN = LBthumnailImgs.find(tn => localhost ? tn.firstElementChild.src.split('http://localhost:3000').filter(el => el !== '').join('').includes(currentImgLB) : tn.firstElementChild.src.includes(currentImgLB))
     currTN.classList.add('active')
     LBactiveImg = currTN
+} 
+
+
+// ! Cart functionality variables & function
+const quantityEl = document.querySelector('.quantity')
+const addToCartBtn = document.querySelector('#addToCart')
+const cartIcon = document.querySelector('#cartIcon')
+const cartEl = document.querySelector('.cartModal')
+const cartQuantityEl = document.querySelector('.cartQuantity')
+const emptyCartMessageEl = document.querySelector('.emptyCartMsg')
+let appendedHtml = false
+let quantity = 0
+const price = 125
+let total = 0
+
+
+
+
+let decrease = () => {
+    quantity !== 0 && (quantity-- , addToCartBtn.disabled = false)
+    quantity == 0 && (addToCartBtn.disabled = true)
+    quantityEl.textContent = quantity
+}
+
+let increase = () => {
+    quantity++
+    quantity !== 0 && (addToCartBtn.disabled = false)
+    quantityEl.textContent = quantity
+}
+
+cartIcon.addEventListener('click', () => {
+    cartEl.classList.toggle('invisible')
+})
+
+addToCartBtn.addEventListener('click', () => {
+    cartQuantityEl.classList.remove('hidden')
+    cartQuantityEl.textContent = quantity
+    
+    Toastify({
+        text: "Item added to cart",
+        duration: 1000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "hsl(25, 100%, 94%)",
+          color: 'hsl(26, 100%, 55%)',
+        },
+      }).showToast();
+
+    calculateTotal()
+
+    document.querySelector('.emptyCartMsg') && document.querySelector('.emptyCartMsg').remove()
+    document.querySelector('.success-checkmark') && document.querySelector('.success-checkmark').remove()
+    document.querySelector('#orderPlacedText') && document.querySelector('#orderPlacedText').remove()
+
+    if (!appendedHtml) {
+        cartEl.innerHTML += /*html*/ `
+        <div class="cartModal__productInfo px-5 flex items-center justify-between py-3">
+        <img class="w-12 rounded-md" src="./images/image-product-1-thumbnail.jpg" alt="">
+    
+            <div class="info">
+                <p class="text-gray-600"> Fall Limited Edition Sneakers</p>
+                <p class="priceCalc flex gap-x-1 text-gray-600">$125 x <span class="cartCalcQuantity">${quantity}</span><span class="result font-bold text-black">$${total}</span></p>
+            </div>
+    
+            <div onclick="clearCart(this)" class="clearCartBtn cursor-pointer">
+             <img src="./images/icon-delete.svg" alt="">
+            </div>
+       </div>
+       <div class="btnCtn px-5 w-full py-3">
+        <button onclick="checkOut(this)" class="w-full text-white font-bold text-lg rounded-md py-4 my-3">Checkout</button>
+       </div>
+        `;
+        appendedHtml = true
+    } else {
+        document.querySelector('.cartModal__productInfo').remove()
+        document.querySelector('.btnCtn').remove()
+        cartEl.innerHTML += /*html*/ `
+        <div class="cartModal__productInfo px-5 flex items-center justify-between py-3">
+        <img class="w-12 rounded-md" src="./images/image-product-1-thumbnail.jpg" alt="">
+    
+            <div class="info">
+                <p class="text-gray-600"> Fall Limited Edition Sneakers</p>
+                <p class="priceCalc flex gap-x-1 text-gray-600">$125 x <span class="cartCalcQuantity">${quantity}</span><span class="result font-bold text-black">$${total}</span></p>
+            </div>
+    
+            <div onclick="clearCart(this)" class="clearCartBtn cursor-pointer">
+             <img src="./images/icon-delete.svg" alt="">
+            </div>
+       </div>
+       <div class="btnCtn px-5 w-full py-3">
+        <button onclick="checkOut(this)" id="checkOutBtn" class="w-full text-white font-bold text-lg rounded-md py-4 my-3">Checkout</button>
+       </div>
+        `;
+    }
+
+    appendedHtml = true
+})
+
+let calculateTotal = () => {
+    total = price * quantity
+}
+
+let clearCart = (el) => {
+    el.parentElement.remove()
+    document.querySelector('.btnCtn').remove()
+    cartEl.innerHTML += /*html*/`
+    <div class="emptyCartMsg container mx-auto text-center">
+     <p class="font-semibold text-gray-500 p-14">Your cart is empty</p>
+    </div>
+    ` 
+    appendedHtml = false
+    quantity = 0
+    addToCartBtn.disabled = true
+    calculateTotal()
+    renderQuantityElems()
+}
+
+
+let renderQuantityElems = () => {
+    cartQuantityEl.textContent = quantity
+    quantityEl.textContent = quantity
+}
+
+let checkOut = (elem) => {
+    elem.parentElement.previousElementSibling.remove()
+    elem.parentElement.remove()
+    cartEl.innerHTML += `
+    <div class="success-checkmark my-5">
+        <div class="check-icon">
+            <span class="icon-line line-tip"></span>
+            <span class="icon-line line-long"></span>
+            <div class="icon-circle"></div>
+            <div class="icon-fix"></div>
+        </div>
+    </div>
+    <div id="orderPlacedText" class="container mx-auto text-center">
+    <p class="font-semibold text-gray-500 mb-6">Order placed</p>
+   </div>
+    `
+
+    appendedHtml = false
+    quantity = 0
+    addToCartBtn.disabled = true
+    calculateTotal()
+    renderQuantityElems()
 }
